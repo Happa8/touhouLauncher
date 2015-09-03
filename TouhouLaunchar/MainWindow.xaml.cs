a9702c3ecffc0c8be84e71098c408ab361fed528
Happa8 <okadakoutaro@gmail.com>
2015-09-03 23:32:40 +0900

    th08~th15までを追加

--- TouhouLaunchar/MainWindow.xaml.cs
+++ TouhouLaunchar/MainWindow.xaml.cs
@@ -39,5 +39,45 @@ namespace TouhouLaunchar
         {
             System.Diagnostics.Process th07Start = System.Diagnostics.Process.Start("D:\\東方\\東方妖々夢\\th07.exe");
         }
+
+        private void th08_Click(object sender, RoutedEventArgs e)
+        {
+            System.Diagnostics.Process th08Start = System.Diagnostics.Process.Start("D:\\東方\\東方永夜抄\\th08.exe");
+        }
+
+        private void th09_Click(object sender, RoutedEventArgs e)
+        {
+            System.Diagnostics.Process th09Start = System.Diagnostics.Process.Start("D:\\東方\\東方花映塚\\th09.exe");
+        }
+
+        private void th10_Click(object sender, RoutedEventArgs e)
+        {
+            System.Diagnostics.Process th10Start = System.Diagnostics.Process.Start("D:\\東方\\東方風神録\\th10.exe");
+        }
+
+        private void th11_Click(object sender, RoutedEventArgs e)
+        {
+            System.Diagnostics.Process th11Start = System.Diagnostics.Process.Start("D:\\東方\\東方地霊殿\\th11.exe");
+        }
+
+        private void th12_Click(object sender, RoutedEventArgs e)
+        {
+            System.Diagnostics.Process th12Start = System.Diagnostics.Process.Start("D:\\東方\\東方星蓮船\\th12.exe");
+        }
+
+        private void th13_Click(object sender, RoutedEventArgs e)
+        {
+            System.Diagnostics.Process th13Start = System.Diagnostics.Process.Start("D:\\東方\\東方神霊廟\\th13.exe");
+        }
+
+        private void th14_Click(object sender, RoutedEventArgs e)
+        {
+            System.Diagnostics.Process th14Start = System.Diagnostics.Process.Start("D:\\東方\\東方輝針城\\th14.exe");
+        }
+
+        private void th15_Click(object sender, RoutedEventArgs e)
+        {
+            System.Diagnostics.Process th15Start = System.Diagnostics.Process.Start("D:\\東方\\東方紺珠伝\\th15.exe");
+        }
     }
 }
